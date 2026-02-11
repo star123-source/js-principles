4257a749e13b7a283ed0dcd23ab8bfb639ea823d
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2026-02-11 11:52:12 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+let nome = "Beatriz";
+let idade = 17;
+let objeto = {nome: "caderno",
+                   cor: "Prata"};
+let fila = [1,2,3,4,5,6,7];
+const valor = "absoluto";
+var funcao = "usado para funções";
+
+
+// variáveis do mesmo tipo, podem ser declaradas numa linha
+var number1 = 10, number2 = 20, number3 = 30;  
+
+let number4 = 40, number5 = 50, number6 = 60;  
+
+let name1 = "Steve", name2 = "John", name3 = "Joe";
+
+let soma = number1+number5+number3;
+console.log(soma);
+
+let valor1 = number4 + 2;
+let valor2 = number6 + 5;
+
+// Estruturas condicionais (`if/else`)
+if(valor1 < valor2){
+  console.log("Valor1 é menor que o Valor2")}else{
+  console.log("Valor2 é menor que o Valor")}
+
+if(number4 == "40"){
+  console.log("Valor ${number4}")
+  console.log(`Valor ${number4}`)
+}
+
+
+Adicionar comentário para a turma...
